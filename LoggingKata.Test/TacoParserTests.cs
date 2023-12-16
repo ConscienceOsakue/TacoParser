efe6fdeabf423169b6f6e64d911aa79eb0c9eab3
Conscience Osakue <consciencenosa@gmail.com>
2023-12-15 18:06:01 -0600

    initial commit of answer

--- LoggingKata.Test/TacoParserTests.cs
+++ LoggingKata.Test/TacoParserTests.cs
@@ -20,25 +20,47 @@ namespace LoggingKata.Test
         }
 
         [Theory]
-        [InlineData("34.073638, -84.677017, Taco Bell Acwort...", -84.677017)]
-        //Add additional inline data. Refer to your CSV file.
-        public void ShouldParseLongitude(string line, double expected)
+        [InlineData("34.073638, -84.677017, Taco Bell Acwort...", 34.073638)]
+        [InlineData("34.035985,-84.683302,Taco Bell Acworth...", 34.035985)]
+        //Done - Add additional inline data. Refer to your CSV file.
+        public void ShouldParselatitude(string line, double expected)
         {
-            // TODO: Complete the test with Arrange, Act, Assert steps below.
+            //Done -  TODO: Complete the test with Arrange, Act, Assert steps below.
             //       Note: "line" string represents input data we will Parse 
             //       to extract the Longitude.  
             //       Each "line" from your .csv file
             //       represents a TacoBell location
 
             //Arrange
+            var tacoParseInsatance = new TacoParser();
 
             //Act
+            var actual = tacoParseInsatance.Parse(line);
 
             //Assert
+            Assert.Equal(expected, actual.Location.Latitude); 
         }
 
 
         //TODO: Create a test called ShouldParseLatitude
+        [Theory]
+        [InlineData("34.073638, -84.677017, Taco Bell Acwort...", -84.677017)]
+        [InlineData("34.035985,-84.683302,Taco Bell Acworth...", -84.683302)]
+        //Done - Add additional inline data. Refer to your CSV file.
+        public void ShouldParseLongitude(string line, double expected)
+        {
+          
+
+            //Arrange
+            var tacoParseInsatance = new TacoParser();
+
+            //Act
+            var actual = tacoParseInsatance.Parse(line);
+
+            //Assert
+            Assert.Equal(expected, actual.Location.Longitude);
+        }
+
 
     }
 }
